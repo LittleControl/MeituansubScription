4a788b5f982a9ec2bffc7bf9afb1d1f96f2aad00
LittleControl <littlecontrol@qq.com>
2019-06-18 18:10:34 +0800

    用户登录界面的优化

--- src/js/all_index.js
+++ src/js/all_index.js
@@ -1,8 +1,8 @@
 var count = 1;
-var wrap = document.getElementsByClassName('wrap')[0]
+var bg = document.getElementsByClassName('bg')[0]
 var time0 = setInterval(function(){
     var url = 'url(../src/img/bg_'+count+'.jpg)'
-    wrap.style.backgroundImage = url
+    bg.style.backgroundImage = url
     count++
     if(count === 6){
         count = 1
